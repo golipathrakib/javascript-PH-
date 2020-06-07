12297fc41ce45bef57e7352d233bfb39287114cc
golipathrakib <rakib.488125@gmail.com>
2020-06-08 01:36:29 +0600

    javascriptconditional

---
+++ condi.js
@@ -0,0 +1,26 @@
+// source: https://www.javascripttutorial.net/javascript-if-else/
+
+/*let x = 25;
+if( x > 10){
+    console.log('x is greater than 10');
+}*/
+
+/*let x = 5;
+if( x > 10){
+    console.log('x is greater than 10');
+} else{
+    console.log('x is less than or equal 10');
+}*/
+
+
+
+let a = 10, b = 10;
+if (a > b){
+    console.log('a is greater than b');
+}else if(a != b) {
+    console.log('a is less than b');
+}else if ( a = b){
+    console.log('a is equal to b');
+}else{
+    console.log('a is never equal to b');
+}
