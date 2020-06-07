41472a27d8bf13909fcb02d5e1552e401886abb8
golipathrakib <rakib.488125@gmail.com>
2020-06-07 23:56:23 +0600

    javaScript Learning

--- hello.js
+++ hello.js
@@ -1,1 +1,11 @@
-<h1>This is javascript</h1>
+
+var bananaPrice = 12;
+console.log(typeof bananaPrice);
+var seenAfter = 21;
+console.log(typeof seenAfter);
+var name="Rakibul Islam";
+console.log(typeof name);
+var isHOt = true;
+var isRich = false;
+console.log(typeof isHOt);
+console.log(typeof isRich);
