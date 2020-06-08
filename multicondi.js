9a7cfa930e737541dc62e66f2de9b8de85c30032
golipathrakib <rakib.488125@gmail.com>
2020-06-09 01:24:02 +0600

    multicondi in javascript

---
+++ multicondi.js
@@ -0,0 +1,28 @@
+/*var jobPaiso = true;
+var savingAmount = 5000;
+
+if(jobPaiso == true && savingAmount > 200000){
+    console.log("Aso tomar patri khuji");
+}else{
+    console.log("Tor kopale biya nai");
+}*/
+
+/*var jobPaiso = false;
+var savingAmount = 500;
+
+if(jobPaiso == true || savingAmount > 200000){
+    console.log("Aso tomar patri khuji");
+}else{
+    console.log("Tor kopale biya nai");
+}*/
+
+var marks = 40;
+if( marks >= 60){
+    console.log("Frist Division");
+}else if( marks >= 45){
+    console.log("Second Division");
+}else if (marks >=33){
+    console.log("Third Division");
+}else{
+    console.log("Fail");
+}
