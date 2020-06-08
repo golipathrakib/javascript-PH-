9ea5538c826a81a93446b3d7c0bf6886bd919265
golipathrakib <rakib.488125@gmail.com>
2020-06-09 02:09:37 +0600

    arrayPrimaryConcept

---
+++ array.js
@@ -0,0 +1,8 @@
+var friendAge = [15, 17, 14, 16];
+var aupu = friendAge[0];
+friendAge[1] = 35;
+var position = friendAge.indexOf(100);
+//console.log(aupu);
+//console.log(friendAge[3]);
+//console.log(friendAge);
+console.log(position);
