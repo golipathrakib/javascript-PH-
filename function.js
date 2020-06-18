8f0958a4938086fdb787747091ba843c4a15f214
golipathrakib <rakib.488125@gmail.com>
2020-06-19 02:14:38 +0600

    function

---
+++ function.js
@@ -0,0 +1,10 @@
+function sayLoveYou(){
+    console.log('Jann o baby');
+    console.log("Billy bay ...Koi Tumi!");
+}
+sayLoveYou();
+var date = 14;
+var place = "Restaurant";
+sayLoveYou()
+var hangoutPlace = 'TSC';
+sayLoveYou();
