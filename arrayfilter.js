295bb9a121744903879d1f59f573d398623bb533
golipathrakib <rakib.488125@gmail.com>
2020-07-15 01:00:50 +0600

    arrayfilter

---
+++ arrayfilter.js
@@ -0,0 +1,7 @@
+    /*var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var result = numbers.filter(number => number > 5);
+    console.log(result);*/
+
+    var words = ["Rakib", "Mithun", "Suzon", "Habib", "Japu", "Islam", "Shobahan"];
+    var result = words.filter(word => word.length> 5);
+    console.log(result);
---
+++ swap.js
@@ -0,0 +1,16 @@
+var a = 5;
+var b = 7;
+console.log("before swap: a =", a, "b =", b);
+
+var temp = a;
+a = b;
+b = temp;
+console.log("after swap: a=", a, "b =",b);
+
+var x = 5;
+var y = 7;
+x = x + y;
+Y = x - y;
+x = x - y;
+
+console.log("After swap: x =", x, "y=", y);
